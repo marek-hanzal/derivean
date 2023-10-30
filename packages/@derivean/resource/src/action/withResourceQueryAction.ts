8d7fc38a6166907acc2e2e96bfe2231d04f98aab
Marek Hanzal <marek.hanzal@x32.cz>
2023-10-30 17:11:44 +0100

    Improved action stuff

--- packages/@derivean/resource/src/action/withResourceQueryAction.ts
+++ packages/@derivean/resource/src/action/withResourceQueryAction.ts
@@ -1,22 +1,23 @@
 "use server";
 
-import {type ArraySchema}    from "@use-pico2/schema";
+import {schema}              from "@use-pico2/schema";
 import {withAction}          from "@use-pico2/server";
 import {ResourceQuerySchema} from "../schema/ResourceQuerySchema";
 import {ResourceSchema}      from "../schema/ResourceSchema";
 
-export const withResourceQueryAction = withAction<
-    ResourceQuerySchema,
-    ArraySchema<ResourceSchema>
->(async query => {
-    return [
-        {
-            id:   "1234",
-            name: "foo",
-        },
-        {
-            id:   "12344",
-            name: "bar",
-        },
-    ];
+export const withResourceQueryAction = withAction({
+    request:  ResourceQuerySchema,
+    response: schema(z => z.array(ResourceSchema)),
+    action:   async query => {
+        return [
+            {
+                id:   "1234",
+                name: "foo",
+            },
+            {
+                id:   "12344",
+                name: "bar",
+            },
+        ];
+    },
 });
--- packages/@use-pico2/server/src/server/withAction.ts
+++ packages/@use-pico2/server/src/server/withAction.ts
@@ -5,6 +5,15 @@ import {
 } from "@use-pico2/schema";
 
 export namespace withAction {
+    export interface Props<
+        TRequestSchema extends RequestSchema,
+        TResponseSchema extends ResponseSchema,
+    > {
+        request: TResponseSchema;
+        response: TResponseSchema;
+        action: Action<TRequestSchema, TResponseSchema>;
+    }
+
     export type Action<
         TRequestSchema extends RequestSchema,
         TResponseSchema extends ResponseSchema,
@@ -15,7 +24,11 @@ export const withAction = <
     TRequestSchema extends RequestSchema,
     TResponseSchema extends ResponseSchema,
 >(
-    handler: withAction.Action<TRequestSchema, TResponseSchema>,
+    {
+        request,
+        response,
+        action,
+    }: withAction.Props<TRequestSchema, TResponseSchema>,
 ): withAction.Action<TRequestSchema, TResponseSchema> => {
-    return async request => handler(request);
+    return async request => action(request);
 };
