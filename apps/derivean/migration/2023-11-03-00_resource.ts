02974313d57ef72ff5f3bfb741267974c5572bea
Marek Hanzal <marek.hanzal@x32.cz>
2023-11-03 01:26:24 +0100

    Some magick about buildings

--- apps/derivean/migration/2023-11-03-00_resource.ts
+++ apps/derivean/migration/2023-11-03-00_resource.ts
@@ -4,7 +4,11 @@ import {Kysely}        from "kysely";
 export async function up(db: Kysely<any>): Promise<void> {
     await withUuidTable("Resource", db)
         .addColumn("name", "varchar(64)", col =>
-            col.notNull().unique()
+            col.notNull()
         )
+        .addColumn("type", "varchar(64)", col =>
+            col.notNull()
+        )
+        .addUniqueConstraint("name_type", ["name", "type"])
         .execute();
 }
--- packages/@derivean/resource/src/api/IResource.ts
+++ packages/@derivean/resource/src/api/IResource.ts
@@ -1,3 +1,10 @@
 export interface IResource {
+    /**
+     * Name of a resource (like log, gem, gold or even more abstract things).
+     */
     name: string;
+    /**
+     * Type of resource; could be resource, building, item, ...
+     */
+    type: string;
 }
