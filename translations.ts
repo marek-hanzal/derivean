a2e6fcabacc655f440747777c5417afa5cea720f
Marek Hanzal <marek.hanzal@x32.cz>
2023-11-05 18:53:21 +0100

    Oops

--- translations.ts
+++ translations.ts
@@ -3,15 +3,16 @@ import {withTx} from "@use-pico/i18n";
 withTx({
     packages: [
         "./apps/derivean",
-    // "./packages/@derivean/building",
-    // "./packages/@derivean/game",
-    // "./packages/@derivean/inventory",
-    // "./packages/@derivean/manager",
-    // "./packages/@derivean/orm",
-    // "./packages/@derivean/producer",
-    // "./packages/@derivean/public",
-    // "./packages/@derivean/resource",
-    // "./packages/@derivean/root",
-    // "./packages/@derivean/server",
-    // "./packages/@derivean/ui",
+        // "./packages/@derivean/building",
+        // "./packages/@derivean/game",
+        // "./packages/@derivean/inventory",
+        // "./packages/@derivean/manager",
+        // "./packages/@derivean/orm",
+        // "./packages/@derivean/producer",
+        // "./packages/@derivean/public",
+        // "./packages/@derivean/resource",
+        // "./packages/@derivean/root",
+        // "./packages/@derivean/server",
+        // "./packages/@derivean/ui",
+    ]
 });
