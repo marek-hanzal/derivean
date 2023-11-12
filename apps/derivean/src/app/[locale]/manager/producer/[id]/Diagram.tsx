abe857290d1173fd21ed43316643a1f6cc032609
Marek Hanzal <marek.hanzal@x32.cz>
2023-11-12 04:03:23 +0100

    Base diagram stuff

---
+++ apps/derivean/src/app/[locale]/manager/producer/[id]/Diagram.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import {type FC} from "react";
+
+export namespace Diagram {
+    export interface Props {
+    }
+}
+
+export const Diagram: FC<Diagram.Props> = () => {
+    return null;
+};
