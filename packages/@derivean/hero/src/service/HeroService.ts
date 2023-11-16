0d3182894fd0e78332924a46dc9831f121a6b611
Marek Hanzal <marek.hanzal@icloud.com>
2023-11-16 21:37:45 +0100

    So, found random gen. for .... everything

--- packages/@derivean/hero/src/service/HeroService.ts
+++ packages/@derivean/hero/src/service/HeroService.ts
@@ -1,21 +1,15 @@
-import Fakerator           from "fakerator";
+import {
+    allFakers,
+    faker
+}                          from "@faker-js/faker";
 import {type IHeroService} from "../api/IHeroService";
 
 export class HeroService implements IHeroService {
     public async random(): Promise<IHeroService.RandomResult> {
-        const fakerator = Fakerator("en-US");
+        const $faker = allFakers[faker.helpers.arrayElement(Object.keys(allFakers)) as keyof allFakers];
         return {
             hero:  {
-                name:     Fakerator(
-                    fakerator.random.arrayElement([
-                        "en-US",
-                        "cs-CZ",
-                        "de-DE",
-                        "sv-SE",
-                        "it-IT",
-                        "hu-HU",
-                    ])
-                ).names.name(),
+                name: $fake.names.name(),
                 health:   100,
                 level:    0,
                 prestige: 0,
