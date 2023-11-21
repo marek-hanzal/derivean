fc1d24220eaa6fb459d3b7656c1397c7e6b0d2dc
Marek Hanzal <marek.hanzal@x32.cz>
2023-11-21 18:23:31 +0100

    ...

--- packages/@use-pico/client/src/ui/Header.tsx
+++ packages/@use-pico/client/src/ui/Header.tsx
@@ -45,7 +45,7 @@ export const Header: FC<Header.Props> = (
     return <div
         className={cn([
             "px-3 py-2 mb-6",
-            "bg-zinc-100",
+            "bg-zinc-50",
             "shadow-md shadow-zinc-300",
             "flex flex-row items-center",
         ])}
