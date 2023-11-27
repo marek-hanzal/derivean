d0dd00c5bf61ba30596470f27993dd56daf87aed
Marek Hanzal <marek.hanzal@x32.cz>
2023-11-27 16:48:06 +0100

    ...

--- packages/@use-pico/utils/src/utils/hash.ts
+++ packages/@use-pico/utils/src/utils/hash.ts
@@ -2,10 +2,21 @@ import coolHash from "object-hash";
 
 /**
  * Generates a hash from the given input (could be even object or function).
+ *
+ * Useful when you want to listen on "useEffect" with an object as a dependency. Be careful as this method
+ * could be quite slow, even it's somehow optimized.
  */
 export const hash = (value: any) => {
-    return coolHash(value, {
-        algorithm: "sha1",
-        encoding:  "hex",
-    });
+    try {
+        return JSON.stringify(value);
+    } catch (e) {
+        return coolHash(value, {
+            algorithm:                 "sha1",
+            encoding:                  "hex",
+            ignoreUnknown:             true,
+            respectType:               false,
+            respectFunctionProperties: false,
+            unorderedArrays:           true,
+        });
+    }
 };
