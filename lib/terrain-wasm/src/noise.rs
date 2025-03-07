873ec08f096981ab5d4780e67ce49698d49443b7
Marek Hanzal <marek.hanzal@icloud.com>
2025-03-07 20:49:59 +0100

    Experiment with modules

---
+++ lib/terrain-wasm/src/noise.rs
@@ -0,0 +1,12 @@
+// https://github.com/Razaekel/noise-rs/blob/develop/examples/complexplanet.rs
+
+pub struct Noise {
+    //
+}
+
+impl Noise {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Noise {
+        Noise {}
+    }
+}
