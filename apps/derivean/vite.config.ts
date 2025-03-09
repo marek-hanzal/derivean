027cd5e3951680a06a30386cb92574739a4e2d6f
Marek Hanzal <marek.hanzal@icloud.com>
2025-03-09 19:14:48 +0100

    Version bump

--- apps/derivean/vite.config.ts
+++ apps/derivean/vite.config.ts
@@ -10,8 +10,6 @@ import tla from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 import paths from "vite-tsconfig-paths";
 
-const host = process.env.TAURI_DEV_HOST;
-
 export default defineConfig({
 	clearScreen: false,
 	plugins: [
@@ -28,10 +26,8 @@ export default defineConfig({
 		tailwindcss(),
 	],
 	worker: { format: "es", plugins: () => [paths(), wasm()] },
-	envPrefix: ["VITE_", "TAURI_ENV_*"],
 	server: {
 		strictPort: true,
-		host: host || false,
 		port: 4040,
 		headers: {
 			"Cross-Origin-Opener-Policy": "same-origin",
